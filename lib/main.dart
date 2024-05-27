169b3bfd488dac4ade277da96a1283fe2ee69a8f
AtulSinghSk <atul@shopkirana.com>
2024-05-27 16:24:22 +0530

    Scroll Pagination

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:scale_up_module/shared_preferences/SharedPref.dart';
-import 'package:scale_up_module/view/Bank_details_screen/BankDetailsScreen.dart';
+import 'package:scale_up_module/utils/constants.dart';
 import 'package:scale_up_module/view/checkoutView/CheckOutOtpScreen.dart';
-import 'package:scale_up_module/view/dashboard_screen/bottom_navigation.dart';
-import 'package:scale_up_module/view/dashboard_screen/vendorDetail/vendor_detail_screen.dart';
 import 'package:scale_up_module/view/splash_screen/SplashScreen.dart';
-import 'DemoScroll.dart';
 import 'data_provider/DataProvider.dart';
-import 'demo1.dart';
 import 'view/agreement_screen/Agreementscreen.dart';
 
 var mobileNumber = "";
@@ -44,6 +40,7 @@ class _MyAppState extends State<MyApp> {
   var product = "";
   var isPayNow = false;
   var transactionId = "";
+  var baseUrl = "";
 
   @override
   void initState() {
@@ -78,9 +75,9 @@ class _MyAppState extends State<MyApp> {
       return CheckOutOtpScreen(transactionId: transactionId);
     } else if (mobileNumber.isNotEmpty) {
       return SplashScreen(
-        mobileNumber: mobileNumber,
-        companyID: company,
-        productID: product);
+          mobileNumber: mobileNumber,
+          companyID: company,
+          productID: product);
     } else {
       return EmptyContainer();
     }
@@ -88,7 +85,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-   /* MyApp.platform.setMethodCallHandler((MethodCall call) async {
+    /* MyApp.platform.setMethodCallHandler((MethodCall call) async {
       if (call.method == "logout") {
         return doSomething();
       }else{
@@ -105,8 +102,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: MyHomePage1(),
-      /*FutureBuilder<void>(
+      home: FutureBuilder<void>(
         future: _initializeData(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -115,19 +111,18 @@ class _MyAppState extends State<MyApp> {
             return Scaffold(
                 body: Center(child: Text('Error: ${snapshot.error}')));
           } else {
-            //return _buildHome();
+            return _buildHome();
 
-           // return SplashScreen(mobileNumber: "8989804393", companyID: "CN_1", productID: "CreditLine");
+            // SplashScreen(mobileNumber: "7803994667", companyID: "CN_1", productID: "CreditLine", isLoggedIn: true));
             //return CheckOutOtpScreen(transactionId: "202457");
-           // return PaymentConfirmation(transactionReqNo: "202457",customerName: "Aarti Mukati",imageUrl:"https://csg10037ffe956af864.blob.core.windows.net/scaleupfiles/0d625556-7f61-47c9-a522-8fef21215b14.jpg",customerCareMoblie: "6263246384",customerCareEmail: "customer.care@scaleupfin.com");
+            // return PaymentConfirmation(transactionReqNo: "202457",customerName: "Aarti Mukati",imageUrl:"https://csg10037ffe956af864.blob.core.windows.net/scaleupfiles/0d625556-7f61-47c9-a522-8fef21215b14.jpg",customerCareMoblie: "6263246384",customerCareEmail: "customer.care@scaleupfin.com");
             //return CongratulationScreen();
             //return SplashScreen(mobileNumber: "6263246384", companyID: "CN_1", ProductID: "CreditLine",);
-            //return SplashScreen(mobileNumber: "8989804393", companyID: "CN_1", productID: "CreditLine");
-           // return BankDetailsScreen(activityId: 2, subActivityId: 2);
-            return MyHomePage1();
+            // return SplashScreen(mobileNumber: "9399927443", companyID: "CN_1", productID: "CreditLine");
+            //return ShowOffersScreen(activityId: 2, subActivityId: 2);
           }
         },
-      ),*/
+      ),
     );
   }
 
@@ -145,15 +140,16 @@ class _MyAppState extends State<MyApp> {
     }
 
     if (jData != null) {
+      final prefsUtil = await SharedPref.getInstance();
+      prefsUtil.saveString(BASE_URL, jData['baseUrl']);
       setState(() {
         mobileNumber = jData['mobileNumber'] ?? "";
         company = jData['companyID'] ?? "";
         product = jData['productID'] ?? "";
+        baseUrl = jData['baseUrl'] ?? jData['baseUrl'];
         isPayNow = jData['isPayNow'] ?? false;
         transactionId = jData['transactionId'] ?? jData['transactionId'];
       });
-
-      print("Mobile Number"+mobileNumber);
     }
   }
 }
